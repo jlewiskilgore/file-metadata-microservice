6381536863f929c9483a2bb8f627181524c151bc
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-05-26 20:53:51 -0400

    Form makes POST request to return file size

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,15 @@
 var path = require('path');
+var multer = require('multer');
+
+var userFile = multer().single('selectedFile');
 
 module.exports = function(app, env) {
 	app.get('/', function(req, res) {
 		res.sendFile(path.join(__dirname + '/../../public/index.html'));
+	});
+
+	app.post('/getfilesize', userFile, function(req, res) {
+		res.json({ "fileSize": req.file.size });
 	});
 
 	app.get('*', function(req, res) {
